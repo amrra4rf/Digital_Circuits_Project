782b37ab78721e86225cd6f4175904b4959db8de
Amr Ashraf <amr200000004@gmail.com>
2025-05-11 16:40:35 +0300

    Intial Files

---
+++ Digital_Project.srcs/sources_1/new/arithmetic_unit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/11/2025 04:27:38 PM
+// Design Name: 
+// Module Name: arithmetic_unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module arithmetic_unit(
+
+    );
+endmodule
